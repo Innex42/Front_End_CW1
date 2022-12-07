76f04636f9d18aee0b7aa7c37e8db29553cdb76f
Innex42 <innex4@gmail.com>
2022-12-07 15:39:32 +0000

    Got a working navbar and cs routing working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Header from './component/Header';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Recipes from './component/Recipes';
 import Menu from './component/Menu';
 import ShoppingList from './component/ShoppingList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Header />}>
+      <Header/>
+        <Routes>        
             <Route path="/" element={<Recipes />} />
             <Route path="Menu" element={<Menu />} />
-            <Route path="ShoppingList" element={<ShoppingList />} />
-          </Route>
+            <Route path="ShoppingList" element={<ShoppingList />} />          
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,127 +1,32 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import {Link, Outlet} from "react-router-dom";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {LinkContainer} from 'react-router-bootstrap'
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 
-
-const pages = ['Recipes', 'Menu', 'Shopping List'];
-//const links = ['Recipes','Menu','ShoppingList']
-const links = [<Link to='/'>Recipes</Link>, <Link to='/Menu'>Menu</Link>, <Link to='ShoppingList'>Shopping List</Link>]
-
-
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-
+function Header() {
   return (
-    <AppBar position="static" style={{background: '#57D837'}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: 'normal',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Recipes R Us
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: 'normal',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Recipes R Us
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                LinkComponent={links[pages.indexOf(page)]}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
+    <Navbar bg='success' expand="lg" variant='dark' >
+      <Container>
+        <Navbar.Brand to="/">Recipes R Us</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Recipes</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/Menu">
+              <Nav.Link to="/Menu">Menu</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/ShoppingList">
+              <Nav.Link to="ShoppingList">ShoppingList</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-    </AppBar>
+    </Navbar>
   );
 }
-export default ResponsiveAppBar;
+
+export default Header;
